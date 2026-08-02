e7059a2f8b07e7a751f94cee8196b9b329e1d0b7
Jasper Caballero <jvicaballero@gmail.com>
2026-08-02 12:13:55 -0400

    including some cleanup here of some placeholders I saw

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,16 @@
-import React from 'react';
-import { useRoutes } from 'react-router-dom';
-import Profile from './pages/Profile.jsx';
-import TripDestinations from './pages/TripDestinations.jsx';
+import React from "react";
+import { useRoutes } from "react-router-dom";
+import Profile from "./pages/Profile.jsx";
+import TripDestinations from "./pages/TripDestinations.jsx";
 
-// TODO: replace with real pages/components (Navigation, Home, Trips, TripDetails, etc.)
 const App = () => {
   let element = useRoutes([
-    { path: '/', element: <h1>Ramblr 🧳 — placeholder home page</h1> },
-    { path: '/profile', element: <Profile /> },
-    { path: '/trips/:tripId', element: <TripDestinations /> },
+    { path: "/", element: <h1>Ramblr 🧳 — placeholder home page</h1> },
+    { path: "/profile", element: <Profile /> },
+    { path: "/trips/:tripId", element: <TripDestinations /> },
   ]);
 
-  return (
-    <div className="app">
-      {element}
-    </div>
-  );
+  return <div className="app">{element}</div>;
 };
 
 export default App;
